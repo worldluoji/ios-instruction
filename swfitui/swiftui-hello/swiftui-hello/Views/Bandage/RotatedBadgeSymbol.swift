c321364409e3c1aecd2b81b8a6c65b00c91a6715
worldluoji <qq1132416@outlook.com>
2024-11-11 16:38:37 +0800

    docs: add annotation

--- swfitui/swiftui-hello/swiftui-hello/Views/Bandage/RotatedBadgeSymbol.swift
+++ swfitui/swiftui-hello/swiftui-hello/Views/Bandage/RotatedBadgeSymbol.swift
@@ -10,6 +10,17 @@ import SwiftUI
 struct RotatedBadgeSymbol: View {
     let angle: Angle
 
+    /*
+    BadgeSymbol()：
+    创建一个 BadgeSymbol 视图。
+    
+    .padding(-60)：
+    使用负的 padding 值来缩小视图的大小。负的 padding 值会使视图的内容向内收缩。
+    
+    .rotationEffect(angle, anchor: .bottom)：
+    将 BadgeSymbol 视图旋转 angle 指定的角度。
+    旋转的中心点是视图的底部（.bottom）。
+    */
     var body: some View {
         BadgeSymbol()
             .padding(-60)
