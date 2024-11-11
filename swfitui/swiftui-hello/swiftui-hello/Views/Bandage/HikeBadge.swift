2c96741b4afc9a3a086280f7fa0be68e41c9a3af
worldluoji <qq1132416@outlook.com>
2024-11-11 16:33:19 +0800

    docs: add annotation

--- swfitui/swiftui-hello/swiftui-hello/Views/Bandage/HikeBadge.swift
+++ swfitui/swiftui-hello/swiftui-hello/Views/Bandage/HikeBadge.swift
@@ -11,6 +11,17 @@ struct HikeBadge: View {
     var name: String
 
     var body: some View {
+        /*
+         第一次 frame 修饰符：
+         .frame(width: 300, height: 300) 设置 Badge 视图的布局大小为 300x300。
+         
+         scaleEffect 修饰符：
+         .scaleEffect(1.0 / 3.0) 将 Badge 视图的内容缩放到原来的 1/3。
+         经过 scaleEffect(1.0 / 3.0) 之后，内容会被缩放到 100x100，但视图本身的布局大小仍然是 300x300。
+         
+         第二次 frame 修饰符：
+         .frame(width: 100, height: 100) 将 Badge 视图的布局大小重新设置为 100x100。
+         */
         VStack(alignment: .center) {
             Badge()
                 .frame(width: 300, height: 300)
